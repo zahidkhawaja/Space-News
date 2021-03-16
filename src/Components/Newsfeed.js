c5050950ee49aa2cfb39f0fa4da869769bb45e70
Zahid Khawaja <zahidkhawaja@icloud.com>
2021-03-16 19:48:17 -0400

    Implement progress indicator from Material-UI

--- src/Components/Newsfeed.js
+++ src/Components/Newsfeed.js
@@ -1,13 +1,23 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import moment from "moment";
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 const NewsFeed = () => {
 
     const [news, setNews] = useState([]);
 
+    // Useful function to intentionally postpone the axios call so we can test the progress indicator
+    // function delay(ms) {
+    //     return function(x) {
+    //       return new Promise(resolve => setTimeout(() => resolve(x), ms));
+    //     };
+    //   }
+
     useEffect(() => {
         axios.get("https://www.spaceflightnewsapi.net/api/v2/articles")
+        // Call the delay function
+        // .then(delay(3000))
         .then(res => {
             setNews(res.data);
         })
@@ -15,6 +25,8 @@ const NewsFeed = () => {
     }, [])
 
     return(
+        <div>
+            {news.length === 0 ? <CircularProgress /> : null}
         <div className = "news">
         <div className = "newsfeed">
             {news.map(article => (
@@ -23,6 +35,7 @@ const NewsFeed = () => {
                     <p className = "timestamp">{moment(article.publishedAt).format("dddd, MMMM Do YYYY")}</p>
                     </div>
             ))}
+        </div>
         </div>
         </div>
     )
