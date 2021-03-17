1bace0e0dadf4c4a6059c7f4fae2bf5d2688987d
Zahid Khawaja <zahidkhawaja@icloud.com>
2021-03-17 11:31:38 -0400

    Comment-out delay func to resolve compiler warning

--- src/Components/Newsfeed.js
+++ src/Components/Newsfeed.js
@@ -8,11 +8,11 @@ const NewsFeed = () => {
     const [news, setNews] = useState([]);
 
     // Useful function to intentionally postpone the axios call so we can test the progress indicator
-    function delay(ms) {
-        return function(x) {
-          return new Promise(resolve => setTimeout(() => resolve(x), ms));
-        };
-      }
+    // function delay(ms) {
+    //     return function(x) {
+    //       return new Promise(resolve => setTimeout(() => resolve(x), ms));
+    //     };
+    //   }
 
     useEffect(() => {
         axios.get("https://www.spaceflightnewsapi.net/api/v2/articles")
