bd4f63a123bf887d2ad6ac8548f31d6e5b5a3cc4
Zahid Khawaja <zahidkhawaja@icloud.com>
2021-07-01 11:36:43 -0400

    Add clarification for delay function

--- src/Components/Newsfeed.js
+++ src/Components/Newsfeed.js
@@ -7,6 +7,7 @@ const NewsFeed = () => {
 
     const [news, setNews] = useState([]);
 
+    // THIS IS FOR TESTING THE LOADING INDICATOR FUNCTIONALITY (emulating slow fetching)
     // function delay(ms) {
     //     return function(x) {
     //       return new Promise(resolve => setTimeout(() => resolve(x), ms));
